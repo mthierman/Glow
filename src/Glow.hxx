6d18f5906505f1f6a5bf94338ca13d2d374f1063
mthierman <mthierman@gmail.com>
2023-08-29 22:58:58 -0600

    Fix up window class

--- src/Glow.hxx
+++ src/Glow.hxx
@@ -5,6 +5,96 @@
 
 namespace glow
 {
+std::string to_string(std::wstring);
+std::wstring to_wstring(std::string);
+
+struct Window
+{
+    Window(std::string);
+    ~Window();
+    HWND get_hwnd();
+
+  private:
+    HWND hwnd;
+    static __int64 __stdcall WndProc(HWND, UINT, WPARAM, LPARAM);
+};
+
+Window::Window(std::string name)
+{
+    auto className{to_wstring(name)};
+
+    WNDCLASSEXW wcex{sizeof(WNDCLASSEX)};
+    wcex.lpszClassName = className.c_str();
+    wcex.lpszMenuName = className.c_str();
+    wcex.lpfnWndProc = Window::WndProc;
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = GetModuleHandleW(nullptr);
+    wcex.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+    wcex.hCursor = (HCURSOR)LoadImageW(nullptr, (LPCWSTR)IDC_ARROW, IMAGE_CURSOR, 0, 0, LR_SHARED);
+    wcex.hIcon = (HICON)LoadImageW(nullptr, (LPCWSTR)IDI_APPLICATION, IMAGE_ICON, 0, 0,
+                                   LR_SHARED | LR_DEFAULTSIZE);
+    wcex.hIconSm = (HICON)LoadImageW(nullptr, (LPCWSTR)IDI_APPLICATION, IMAGE_ICON, 0, 0,
+                                     LR_SHARED | LR_DEFAULTSIZE);
+
+    auto atom{RegisterClassExW(&wcex)};
+
+    if (atom == 0)
+        MessageBoxW(nullptr, std::to_wstring(GetLastError()).c_str(), L"Error", 0);
+
+    hwnd = CreateWindowExW(0, className.c_str(), className.c_str(), WS_OVERLAPPEDWINDOW,
+                           CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, nullptr,
+                           nullptr, GetModuleHandleW(nullptr), this);
+
+    if (!hwnd)
+        MessageBoxW(nullptr, std::to_wstring(GetLastError()).c_str(), L"Error", 0);
+
+    ShowWindow(hwnd, SW_SHOWDEFAULT);
+}
+
+Window::~Window() {}
+
+HWND Window::get_hwnd() { return hwnd; }
+
+template <class T> T* InstanceFromWndProc(HWND hwnd, UINT msg, LPARAM lparam)
+{
+    T* pInstance;
+
+    if (msg == WM_NCCREATE)
+    {
+        LPCREATESTRUCTW pCreateStruct = reinterpret_cast<LPCREATESTRUCTW>(lparam);
+        pInstance = reinterpret_cast<T*>(pCreateStruct->lpCreateParams);
+        SetWindowLongPtrW(hwnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(pInstance));
+    }
+
+    else
+        pInstance = reinterpret_cast<T*>(GetWindowLongPtrW(hwnd, GWLP_USERDATA));
+
+    return pInstance;
+}
+
+__int64 __stdcall Window::WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+    Window* window = InstanceFromWndProc<Window>(hwnd, msg, lparam);
+
+    if (window)
+    {
+        switch (msg)
+        {
+        case WM_CLOSE:
+            DestroyWindow(hwnd);
+            return 0;
+
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+        }
+    }
+
+    return DefWindowProcW(hwnd, msg, wparam, lparam);
+}
+
 std::string to_string(std::wstring in)
 {
     if (!in.empty())
@@ -28,9 +118,8 @@ std::string to_string(std::wstring in)
     return {};
 }
 
-std::wstring to_wide(std::string in)
+std::wstring to_wstring(std::string in)
 {
-
     if (!in.empty())
     {
         auto inSize{static_cast<int>(in.size())};
@@ -51,44 +140,4 @@ std::wstring to_wide(std::string in)
 
     return {};
 }
-
-std::string bool_to_string(bool in) { return in ? std::string("true") : std::string("false"); }
-
-std::wstring bool_to_wide(bool in) { return in ? std::wstring(L"true") : std::wstring(L"false"); }
-
-void print(std::string in) { OutputDebugStringW(to_wide(in).c_str()); }
-
-void println(std::string in)
-{
-    OutputDebugStringW(to_wide(in).c_str());
-    OutputDebugStringW(L"\n");
-}
-
-void wprint(std::wstring in) { OutputDebugStringW(in.c_str()); }
-
-void wprintln(std::wstring in)
-{
-    OutputDebugStringW(in.c_str());
-    OutputDebugStringW(L"\n");
-}
-
-void msgbox(std::string in) { MessageBoxW(nullptr, to_wide(in).c_str(), L"Message", 0); };
-
-void wmsgbox(std::wstring in) { MessageBoxW(nullptr, in.c_str(), L"Message", 0); };
-
-int error(std::string in)
-{
-    std::wstring error = to_wide(in + ". Error: " + std::to_string(GetLastError()));
-    MessageBoxW(nullptr, error.c_str(), L"Error", 0);
-
-    return 0;
-};
-
-int werror(std::wstring in)
-{
-    std::wstring error = in + L". Error: " + std::to_wstring(GetLastError());
-    MessageBoxW(nullptr, error.c_str(), L"Error", 0);
-
-    return 0;
-};
 } // namespace glow
