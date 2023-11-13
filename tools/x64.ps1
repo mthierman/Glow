28bbc7aeb30504e868ceb56a4b8906aff8b52ae0
mthierman <mthierman@gmail.com>
2023-11-13 00:51:21 -0700

    Add tools

---
+++ tools/x64.ps1
@@ -0,0 +1,3 @@
+$vswhere = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
+$vspath = & $vswhere -products * -latest -property installationPath
+& "$vspath\Common7\Tools\Launch-VsDevShell.ps1" -HostArch amd64 -Arch amd64 -SkipAutomaticLocation
