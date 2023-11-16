55b3f19cab4fc1bdd2c105083cfbefb69aae0e0b
mthierman <mthierman@gmail.com>
2023-11-15 17:44:33 -0700

    Remove settings header

--- tests/Console.cxx
+++ tests/Console.cxx
@@ -19,6 +19,27 @@ using json = nlohmann::json;
 using std::println;
 using namespace std::string_view_literals;
 
+struct settings
+{
+    std::string name;
+    std::string version;
+    int width{0};
+    int height{0};
+};
+
+void to_json(json& j, const settings& s)
+{
+    j = json{{"name", s.name}, {"version", s.version}, {"width", s.width}, {"height", s.height}};
+}
+
+void from_json(const json& j, settings& s)
+{
+    j.at("name").get_to(s.name);
+    j.at("version").get_to(s.version);
+    j.at("width").get_to(s.width);
+    j.at("height").get_to(s.height);
+}
+
 auto main() -> int
 {
     std::println("{}", glow::filesystem::known_folder(FOLDERID_LocalAppData).string());
