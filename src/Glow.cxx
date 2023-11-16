cead1df57044e614f7ab7cc23a23e76d59629a5a
mthierman <mthierman@gmail.com>
2023-11-15 20:13:56 -0700

    Strip quotes in CMakeLists

--- src/Glow.cxx
+++ src/Glow.cxx
@@ -1,3 +1,13 @@
+// clang-format off
+// ╔──────────────────────╗
+// │                      │
+// │     ╔═╗╦  ╔═╗╦ ╦     │  Glow - https://github.com/mthierman/Glow
+// │     ║ ╦║  ║ ║║║║     │  SPDX-FileCopyrightText: © 2023 Mike Thierman <mthierman@gmail.com>
+// │     ╚═╝╩═╝╚═╝╚╩╝     │  SPDX-License-Identifier: MIT
+// │                      │
+// ╚──────────────────────╝
+// clang-format on
+
 #include <console/console.hxx>
 #include <filesystem/filesystem.hxx>
 #include <gui/app.hxx>
--- src/PCH.hxx
+++ src/PCH.hxx
@@ -1,3 +1,13 @@
+// clang-format off
+// ╔──────────────────────╗
+// │                      │
+// │     ╔═╗╦  ╔═╗╦ ╦     │  Glow - https://github.com/mthierman/Glow
+// │     ║ ╦║  ║ ║║║║     │  SPDX-FileCopyrightText: © 2023 Mike Thierman <mthierman@gmail.com>
+// │     ╚═╝╩═╝╚═╝╚╩╝     │  SPDX-License-Identifier: MIT
+// │                      │
+// ╚──────────────────────╝
+// clang-format on
+
 #pragma once
 
 #include <GlowConfig.h>
