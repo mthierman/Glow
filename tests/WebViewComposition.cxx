8a30cbdc290a6c49827bf5e735dedb2360207f9a
mthierman <mthierman@gmail.com>
2023-11-15 19:10:21 -0700

    Fix formatting on copyright banners

--- tests/WebViewComposition.cxx
+++ tests/WebViewComposition.cxx
@@ -1,10 +1,12 @@
+// clang-format off
 // ╔──────────────────────╗
 // │                      │
 // │     ╔═╗╦  ╔═╗╦ ╦     │  Glow - https://github.com/mthierman/Glow
-// │     ║ ╦║  ║ ║║║║     │  SPDX-FileCopyrightText: © 2023 Mike Thierman mthierman@gmail.com
+// │     ║ ╦║  ║ ║║║║     │  SPDX-FileCopyrightText: © 2023 Mike Thierman <mthierman@gmail.com>
 // │     ╚═╝╩═╝╚═╝╚╩╝     │  SPDX-License-Identifier: MIT
 // │                      │
 // ╚──────────────────────╝
+// clang-format on
 
 #include <Windows.h>
 #include <DispatcherQueue.h>
