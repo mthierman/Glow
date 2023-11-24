21d8b424dca2bc7e6dad5378796800d5614cb44e
mthierman <mthierman@gmail.com>
2023-11-23 18:25:47 -0700

    IconConverter: Replace GDI+ with WIC

--- tools/IconConverter.cxx
+++ tools/IconConverter.cxx
@@ -8,29 +8,25 @@
 // ╚──────────────────────╝
 // clang-format on
 
-#ifndef GDIPVER
-#define GDIPVER 0x0110
-#endif
-
 #include <Windows.h>
-#include <gdiplus.h>
+#include <wincodec.h>
+
 #include <wil/com.h>
 #include <wil/resource.h>
 
 #include <algorithm>
+#include <iostream>
 #include <filesystem>
 #include <fstream>
 #include <memory>
-#include <print>
 #include <utility>
 #include <vector>
 
 #include <console/console.hxx>
+#include <filesystem/filesystem.hxx>
 #include <text/text.hxx>
 
-auto get_encoder_clsid(const std::wstring& format, CLSID* pClsid) -> bool;
-auto get_bitmap(const std::filesystem::path& inputCanonical, const int& size, CLSID* pClsid)
-    -> std::vector<char>;
+auto get_bitmap(const std::filesystem::path& inputCanonical, const int& size) -> std::vector<char>;
 auto write_header(std::ofstream& outputStream, uint16_t count) -> void;
 auto write_entry(std::ofstream& outputStream, std::vector<char>& bitmap, uint8_t size,
                  uint32_t offset) -> void;
@@ -38,158 +34,148 @@ auto write_bitmap(std::ofstream& outputStream, std::vector<char>& bitmap) -> voi
 
 auto main() -> int
 {
-    ULONG_PTR gdiplusToken;
-    auto gdiplusStartupInput{std::make_unique<Gdiplus::GdiplusStartupInput>()};
-
-    if (Gdiplus::GdiplusStartup(&gdiplusToken, gdiplusStartupInput.get(), nullptr) !=
-        Gdiplus::Status::Ok)
-        return 0;
-
     auto argv{glow::console::get_argv()};
 
-    if (argv.size() == 3)
+    CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
     {
-        std::filesystem::path inputFile{argv[1]};
-        std::filesystem::path outputFile{argv[2]};
-        std::filesystem::path inputCanonical;
-
-        if (!std::filesystem::exists(inputFile)) return 0;
-
-        try
+        if (argv.size() == 3)
         {
-            inputCanonical = std::filesystem::canonical(inputFile);
-        }
-        catch (std::filesystem::filesystem_error& /*e*/)
-        {
-            return 0;
-        }
+            std::filesystem::path inputFile{argv[1]};
+            std::filesystem::path outputFile{argv[2]};
+            std::filesystem::path inputCanonical;
 
-        if (inputCanonical.empty()) return 0;
+            if (!std::filesystem::exists(inputFile)) return 0;
 
-        auto pngClsid{std::make_unique<CLSID>()};
+            try
+            {
+                inputCanonical = std::filesystem::canonical(inputFile);
+            }
+            catch (std::filesystem::filesystem_error& /*e*/)
+            {
+                return 0;
+            }
 
-        if (!get_encoder_clsid(L"image/png", pngClsid.get())) return 0;
+            if (inputCanonical.empty()) return 0;
 
-        std::vector<std::vector<char>> bitmaps;
-        std::vector<int> bitmapSizes{256, 128, 96, 80, 72, 64, 60, 48, 40, 36, 32, 30, 24, 20, 16};
+            std::vector<std::vector<char>> bitmaps;
+            std::vector<int> bitmapSizes{256, 128, 96, 80, 72, 64, 60, 48,
+                                         40,  36,  32, 30, 24, 20, 16};
 
-        for (auto i = 0; i < bitmapSizes.size(); i++)
-        {
-            bitmaps.push_back(
-                get_bitmap(inputCanonical.wstring().c_str(), bitmapSizes[i], pngClsid.get()));
-        }
+            for (auto i = 0; i < bitmapSizes.size(); i++)
+            {
+                bitmaps.push_back(get_bitmap(inputCanonical.wstring().c_str(), bitmapSizes[i]));
+            }
 
-        std::vector<uint32_t> sizes;
-        sizes.reserve(bitmaps.size());
-        for (auto const& bitmap : bitmaps)
-        {
-            sizes.push_back(static_cast<uint32_t>(bitmap.size()));
-        }
+            std::vector<uint32_t> sizes;
+            sizes.reserve(bitmaps.size());
+            for (auto const& bitmap : bitmaps)
+            {
+                sizes.push_back(static_cast<uint32_t>(bitmap.size()));
+            }
 
-        std::ofstream outputStream;
-        uint16_t count{static_cast<uint16_t>(bitmapSizes.size())};
-        uint32_t offset{6 + (16 * static_cast<uint32_t>(count))};
+            std::ofstream outputStream;
+            uint16_t count{static_cast<uint16_t>(bitmapSizes.size())};
+            uint32_t offset{6 + (16 * static_cast<uint32_t>(count))};
 
-        std::vector<uint32_t> positions;
-        positions.push_back(offset);
-        auto cumulate{offset};
-        for (auto i = 0; i < sizes.size(); ++i)
-        {
-            cumulate += sizes[i];
-            positions.push_back(cumulate);
-        }
+            std::vector<uint32_t> positions;
+            positions.push_back(offset);
+            auto cumulate{offset};
+            for (auto i = 0; i < sizes.size(); ++i)
+            {
+                cumulate += sizes[i];
+                positions.push_back(cumulate);
+            }
 
-        std::vector<uint8_t> dimensions;
-        dimensions.reserve(sizes.size());
-        for (const auto& size : sizes)
-        {
-            dimensions.push_back(static_cast<uint8_t>(size));
-        }
+            std::vector<uint8_t> dimensions;
+            dimensions.reserve(sizes.size());
+            for (const auto& size : sizes)
+            {
+                dimensions.push_back(static_cast<uint8_t>(size));
+            }
 
-        outputStream.open(outputFile, std::ios::binary);
+            outputStream.open(outputFile, std::ios::binary);
 
-        write_header(outputStream, count);
+            write_header(outputStream, count);
 
-        write_entry(outputStream, bitmaps[0], 0, positions[0]);
-        for (int i = 1; i < static_cast<int>(bitmapSizes.size()); i++)
-        {
-            write_entry(outputStream, bitmaps[i], dimensions[i], positions[i]);
-        }
+            write_entry(outputStream, bitmaps[0], 0, positions[0]);
+            for (int i = 1; i < static_cast<int>(bitmapSizes.size()); i++)
+            {
+                write_entry(outputStream, bitmaps[i], dimensions[i], positions[i]);
+            }
 
-        for (int i = 0; i < static_cast<int>(bitmapSizes.size()); i++)
-        {
-            write_bitmap(outputStream, bitmaps[i]);
+            for (int i = 0; i < static_cast<int>(bitmapSizes.size()); i++)
+            {
+                write_bitmap(outputStream, bitmaps[i]);
+            }
         }
     }
-
-    Gdiplus::GdiplusShutdown(gdiplusToken);
+    CoUninitialize();
 
     return 0;
 }
 
-auto get_encoder_clsid(const std::wstring& format, CLSID* pClsid) -> bool
+auto get_bitmap(const std::filesystem::path& inputCanonical, const int& size) -> std::vector<char>
 {
-    UINT num{0};
-    UINT size{0};
+    wil::com_ptr<IWICImagingFactory> pFactory;
+    wil::com_ptr<IWICBitmapDecoder> pDecoder;
+    wil::com_ptr<IWICBitmapFrameDecode> pFrameDecode;
+    wil::com_ptr<IWICBitmapEncoder> pEncoder;
+    wil::com_ptr<IWICBitmapFrameEncode> pFrameEncode;
+    wil::com_ptr<IWICFormatConverter> pFormatter;
+    wil::com_ptr<IWICBitmapScaler> pScaler;
+    wil::com_ptr<IWICStream> pStream;
+    wil::com_ptr<IPropertyBag2> pPropertyBag;
 
-    if (Gdiplus::GetImageEncodersSize(&num, &size) != Gdiplus::Status::Ok) return false;
+    if (FAILED(CoCreateInstance(CLSID_WICImagingFactory, NULL, CLSCTX_INPROC_SERVER,
+                                IID_PPV_ARGS(&pFactory))))
+        return {};
 
-    std::vector<Gdiplus::ImageCodecInfo> codecs(size);
+    if (FAILED(pFactory->CreateDecoderFromFilename(inputCanonical.wstring().c_str(), NULL,
+                                                   GENERIC_READ, WICDecodeMetadataCacheOnDemand,
+                                                   &pDecoder)))
+        return {};
 
-    if (Gdiplus::GetImageEncoders(num, size, codecs.data()) != Gdiplus::Status::Ok) return false;
-
-    for (auto codec : codecs)
-    {
-        if (format == std::wstring{codec.MimeType})
-        {
-            *pClsid = codec.Clsid;
-            return true;
-        }
-    }
-
-    return false;
-}
-
-auto get_bitmap(const std::filesystem::path& inputCanonical, const int& size, CLSID* pClsid)
-    -> std::vector<char>
-{
-    auto src{std::unique_ptr<Gdiplus::Image>(
-        Gdiplus::Image::FromFile(inputCanonical.wstring().c_str()))};
-    auto dst{std::make_unique<Gdiplus::Bitmap>(size, size)};
-    auto g{std::unique_ptr<Gdiplus::Graphics>(Gdiplus::Graphics::FromImage(dst.get()))};
-
-    // https://photosauce.net/blog/post/image-scaling-with-gdi-part-3-drawimage-and-the-settings-that-affect-it
-    g->SetSmoothingMode(Gdiplus::SmoothingModeAntiAlias8x8);
-    g->SetInterpolationMode(Gdiplus::InterpolationModeHighQualityBicubic);
-    g->SetPixelOffsetMode(Gdiplus::PixelOffsetModeHighQuality);
-    g->SetCompositingMode(Gdiplus::CompositingModeSourceOver);
-    g->SetCompositingQuality(Gdiplus::CompositingQualityHighQuality);
-
-    auto attr{std::make_unique<Gdiplus::ImageAttributes>()};
-    attr->SetWrapMode(Gdiplus::WrapModeTileFlipXY);
-    auto rect{Gdiplus::Rect(0, 0, size, size)};
-
-    g->DrawImage(src.get(), rect, 0, 0, src->GetWidth(), src->GetHeight(), Gdiplus::Unit::UnitPixel,
-                 attr.get(), 0, 0);
+    if (FAILED(pDecoder->GetFrame(0, &pFrameDecode))) return {};
 
     wil::unique_hglobal hglobal;
     wil::com_ptr<IStream> istream;
 
     if (FAILED(::CreateStreamOnHGlobal(hglobal.get(), TRUE, &istream))) return {};
 
-    dst->Save(istream.get(), pClsid);
+    if (FAILED(pFactory->CreateBitmapScaler(&pScaler))) return {};
+    if (FAILED(pScaler->Initialize(pFrameDecode.get(), size, size,
+                                   WICBitmapInterpolationModeHighQualityCubic)))
+        return {};
+
+    UINT bytesPerPixel{4};
+    UINT stride{size * bytesPerPixel};
+    UINT bufsize{size * size * bytesPerPixel};
+
+    std::vector<BYTE> scaledBuffer(bufsize);
+    if (FAILED(pScaler->CopyPixels(NULL, stride, bufsize, scaledBuffer.data()))) return {};
+
+    if (FAILED(pFactory->CreateEncoder(GUID_ContainerFormatPng, NULL, &pEncoder))) return {};
+    if (FAILED(pEncoder->Initialize(istream.get(), WICBitmapEncoderNoCache))) return {};
+    if (FAILED(pEncoder->CreateNewFrame(&pFrameEncode, &pPropertyBag))) return {};
+
+    if (FAILED(pFrameEncode->Initialize(pPropertyBag.get()))) return {};
+    if (FAILED(pFrameEncode->SetSize(size, size))) return {};
+    WICPixelFormatGUID pixelFormatDestination{GUID_WICPixelFormat32bppBGRA};
+    if (FAILED(pFrameEncode->SetPixelFormat(&pixelFormatDestination))) return {};
+    if (FAILED(pFrameEncode->WritePixels(size, stride, bufsize, scaledBuffer.data()))) return {};
+
+    if (FAILED(pFrameEncode->Commit())) return {};
+    if (FAILED(pEncoder->Commit())) return {};
 
     if (FAILED(::GetHGlobalFromStream(istream.get(), &hglobal))) return {};
-
-    auto bufsize{::GlobalSize(hglobal.get())};
+    auto vecBufsize{::GlobalSize(hglobal.get())};
     auto* ptr{::GlobalLock(hglobal.get())};
-
     if (ptr == nullptr) return {};
 
     std::vector<char> vec;
-    vec.resize(bufsize);
+    vec.resize(vecBufsize);
 
-    std::copy(static_cast<char*>(ptr), (static_cast<char*>(ptr) + bufsize), vec.begin());
+    std::copy(static_cast<char*>(ptr), (static_cast<char*>(ptr) + vecBufsize), vec.begin());
 
     ::GlobalUnlock(hglobal.get());
 
