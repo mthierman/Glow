18333043fa6f0b05c1ec67d1d3b94f9f08aa14f7
mthierman <mthierman@gmail.com>
2023-11-30 03:34:23 -0700

    Add tests

---
+++ tests/Tests.cxx
@@ -0,0 +1,20 @@
+// clang-format off
+// ╔──────────────────────╗
+// │                      │
+// │     ╔═╗╦  ╔═╗╦ ╦     │  Glow - https://github.com/mthierman/Glow
+// │     ║ ╦║  ║ ║║║║     │  SPDX-FileCopyrightText: © 2023 Mike Thierman <mthierman@gmail.com>
+// │     ╚═╝╩═╝╚═╝╚╩╝     │  SPDX-License-Identifier: MIT
+// │                      │
+// ╚──────────────────────╝
+// clang-format on
+
+#include <console/console.hxx>
+#include <filesystem/filesystem.hxx>
+#include <gui/app.hxx>
+#include <gui/gui.hxx>
+#include <gui/webview_composition.hxx>
+#include <gui/webview.hxx>
+#include <logging/logging.hxx>
+#include <text/text.hxx>
+
+auto main() -> int { return 0; }
