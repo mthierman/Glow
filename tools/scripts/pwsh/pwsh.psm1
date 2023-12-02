5b6ab8067c050f8d8b36334cc23d93c6fb6c253b
mthierman <mthierman@gmail.com>
2023-12-01 17:38:16 -0700

    Test powershell module

--- tools/scripts/pwsh/pwsh.psm1
+++ tools/scripts/pwsh/pwsh.psm1
@@ -1,13 +1,19 @@
-function Initialize-DevShell64
+function Enter-DevShell64
 {
     $vswhere = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
     $vspath = & $vswhere -products * -latest -property installationPath
     & "$vspath\Common7\Tools\Launch-VsDevShell.ps1" -HostArch amd64 -Arch amd64 -SkipAutomaticLocation
 }
 
-function Initialize-DevShell32
+function Enter-DevShell32
 {
     $vswhere = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
     $vspath = & $vswhere -products * -latest -property installationPath
     & "$vspath\Common7\Tools\Launch-VsDevShell.ps1" -HostArch x86 -Arch x86 -SkipAutomaticLocation
+}
+
+function Test-Module
+{
+    Get-Location
+    Get-ChildItem
 }
