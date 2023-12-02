9f2e08b32e737086231287e1157f6c826dc825bf
mthierman <mthierman@gmail.com>
2023-12-02 12:26:01 -0700

    Fix generator expressions for VS generator

--- cmake/Data.cmake
+++ cmake/Data.cmake
@@ -1,17 +1,3 @@
-if(GLOW_BUILD_EXAMPLES)
-    file(
-        GENERATE
-        OUTPUT ${PROJECT_BINARY_DIR}/data/Glow.rc
-        CONTENT "1 ICON \"$<$<CONFIG:Debug>:debug>$<$<CONFIG:Release>:release>.ico\""
-    )
-
-    file(
-        TO_CMAKE_PATH
-        ${PROJECT_BINARY_DIR}/data/Glow.rc
-        GLOW_DATA_RC
-    )
-endif()
-
 file(
     TO_CMAKE_PATH
     ${PROJECT_SOURCE_DIR}/data/Glow.manifest
