46ce8aa2274d030f9315850f1ded0d57dfb51c28
mthierman <mthierman@gmail.com>
2023-12-13 20:39:09 -0700

    Cleanup text

--- glow/text/text.cxx
+++ glow/text/text.cxx
@@ -19,17 +19,17 @@ auto narrow(std::wstring wstring) -> std::string
     {
         auto inSize{static_cast<int>(wstring.size())};
 
-        auto outSize{::WideCharToMultiByte(CP_UTF8, WC_NO_BEST_FIT_CHARS | WC_ERR_INVALID_CHARS,
-                                           wstring.data(), inSize, nullptr, 0, nullptr, nullptr)};
+        auto outSize{WideCharToMultiByte(CP_UTF8, WC_NO_BEST_FIT_CHARS | WC_ERR_INVALID_CHARS,
+                                         wstring.data(), inSize, nullptr, 0, nullptr, nullptr)};
 
         if (outSize > 0)
         {
             std::string out;
             out.resize(static_cast<size_t>(outSize));
 
-            if (::WideCharToMultiByte(CP_UTF8, WC_NO_BEST_FIT_CHARS | WC_ERR_INVALID_CHARS,
-                                      wstring.data(), inSize, out.data(), outSize, nullptr,
-                                      nullptr) > 0)
+            if (WideCharToMultiByte(CP_UTF8, WC_NO_BEST_FIT_CHARS | WC_ERR_INVALID_CHARS,
+                                    wstring.data(), inSize, out.data(), outSize, nullptr,
+                                    nullptr) > 0)
                 return out;
         }
     }
@@ -44,16 +44,16 @@ auto widen(std::string string) -> std::wstring
     {
         auto inSize{static_cast<int>(string.size())};
 
-        auto outSize{::MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, string.data(), inSize,
-                                           nullptr, 0)};
+        auto outSize{
+            MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, string.data(), inSize, nullptr, 0)};
 
         if (outSize > 0)
         {
             std::wstring out;
             out.resize(static_cast<size_t>(outSize));
 
-            if (::MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, string.data(), inSize,
-                                      out.data(), outSize) > 0)
+            if (MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, string.data(), inSize,
+                                    out.data(), outSize) > 0)
                 return out;
         }
     }
