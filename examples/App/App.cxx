023a5fb7dcdf11c85efe8a75fa7c816d20d821f5
mthierman <mthierman@gmail.com>
2023-12-20 16:50:28 -0700

    App window & main window example finalized

--- examples/App/App.cxx
+++ examples/App/App.cxx
@@ -11,17 +11,30 @@
 
 #include <gui/webview.hxx>
 
+using glow::gui::WebView2;
+
 auto WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, int nCmdShow)
     -> int
 {
     App app;
+    glow::gui::set_title(app.m_hwnd.get(), "App");
 
-    std::vector<std::unique_ptr<glow::gui::WebView2>> webviews;
-    webviews.push_back(std::make_unique<glow::gui::WebView2>(app.m_hwnd.get(), 1));
-    webviews.push_back(std::make_unique<glow::gui::WebView2>(app.m_hwnd.get(), 2));
+    std::vector<std::unique_ptr<glow::gui::WebView2>> app_webviews;
+    app_webviews.push_back(std::make_unique<WebView2>(app.m_hwnd.get(), 1));
+    app_webviews.push_back(std::make_unique<WebView2>(app.m_hwnd.get(), 2));
 
-    glow::gui::set_border(webviews.at(0)->m_hwnd.get(), true);
-    glow::gui::set_border(webviews.at(1)->m_hwnd.get(), true);
+    glow::gui::set_border(app_webviews.at(0)->m_hwnd.get(), true);
+    glow::gui::set_border(app_webviews.at(1)->m_hwnd.get(), true);
+
+    Window window;
+    glow::gui::set_title(window.m_hwnd.get(), "Window");
+
+    std::vector<std::unique_ptr<glow::gui::WebView2>> window_webviews;
+    window_webviews.push_back(std::make_unique<WebView2>(window.m_hwnd.get(), 1));
+    window_webviews.push_back(std::make_unique<WebView2>(window.m_hwnd.get(), 2));
+
+    glow::gui::set_border(window_webviews.at(0)->m_hwnd.get(), true);
+    glow::gui::set_border(window_webviews.at(1)->m_hwnd.get(), true);
 
     glow::gui::message_loop();
 
