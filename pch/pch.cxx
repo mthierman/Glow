36345485aedb5e53a31aa09bf0d090d0b3d0be69
mthierman <mthierman@gmail.com>
2024-01-02 00:52:34 -0700

    Remove WebView and PCH

--- pch/pch.cxx
+++
@@ -1,9 +0,0 @@
-// clang-format off
-// ╔──────────────╗
-// │ ╔═╗╦  ╔═╗╦ ╦ │  Glow - https://github.com/mthierman/Glow
-// │ ║ ╦║  ║ ║║║║ │  SPDX-FileCopyrightText: © 2023 Mike Thierman <mthierman@gmail.com>
-// │ ╚═╝╩═╝╚═╝╚╩╝ │  SPDX-License-Identifier: MIT
-// ╚──────────────╝
-// clang-format on
-
-#include "pch.hxx"
