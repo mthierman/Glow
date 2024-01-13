9a8c1e7c11689bb5d8da3e5573da5430fe73986d
mthierman <mthierman@gmail.com>
2024-01-12 19:24:16 -0700

    Break out templates now..

---
+++ Glow/glow/glow.hxx
@@ -0,0 +1,14 @@
+// clang-format off
+// ╔──────────────╗
+// │ ╔═╗╦  ╔═╗╦ ╦ │  Glow - https://github.com/mthierman/Glow
+// │ ║ ╦║  ║ ║║║║ │  SPDX-FileCopyrightText: © 2023 Mike Thierman <mthierman@gmail.com>
+// │ ╚═╝╩═╝╚═╝╚╩╝ │  SPDX-License-Identifier: MIT
+// ╚──────────────╝
+// clang-format on
+
+#pragma once
+
+#include <glow/console.hxx>
+#include <glow/filesystem.hxx>
+#include <glow/gui.hxx>
+#include <glow/text.hxx>
