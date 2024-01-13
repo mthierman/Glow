9fb76262787706525d8944b0f78389461f9774b9
mthierman <mthierman@gmail.com>
2024-01-12 22:02:17 -0700

    Fix include path

--- include/glow/glow.hxx
+++ include/glow/glow.hxx
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "../src/console.hxx"
-#include "../src/filesystem.hxx"
-#include "../src/gui.hxx"
-#include "../src/text.hxx"
-#include "../src/window.hxx"
+#include "../../src/console.hxx"
+#include "../../src/filesystem.hxx"
+#include "../../src/gui.hxx"
+#include "../../src/text.hxx"
+#include "../../src/window.hxx"
