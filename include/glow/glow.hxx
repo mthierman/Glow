b90fa0369bc2a45b10c08ed34bc0667d5e034a8b
mthierman <mthierman@gmail.com>
2024-01-15 11:00:34 -0700

    Add banner to glow.hxx

--- include/glow/glow.hxx
+++ include/glow/glow.hxx
@@ -1,3 +1,11 @@
+// clang-format off
+// ╔──────────────╗
+// │ ╔═╗╦  ╔═╗╦ ╦ │  Glow - https://github.com/mthierman/Glow
+// │ ║ ╦║  ║ ║║║║ │  SPDX-FileCopyrightText: © 2023 Mike Thierman <mthierman@gmail.com>
+// │ ╚═╝╩═╝╚═╝╚╩╝ │  SPDX-License-Identifier: MIT
+// ╚──────────────╝
+// clang-format on
+
 #pragma once
 
 #include "../../src/console.hxx"
