e40fb3b8710ec9566616780c947412c27ae8313c
mthierman <mthierman@gmail.com>
2024-01-21 13:13:55 -0700

    Update SQLite

--- cmake/SQLite.cmake
+++ cmake/SQLite.cmake
@@ -1,13 +1,13 @@
 if(GLOW_LOCAL_LIBS)
     file(
         TO_CMAKE_PATH
-        "$ENV{LIBRARIES}/sqlite-amalgamation-3440200"
+        "$ENV{LIBRARIES}/sqlite-amalgamation-3450000"
         sqlite_SOURCE_DIR
     )
 else()
     FetchContent_Declare(
         sqlite
-        URL https://www.sqlite.org/2023/sqlite-amalgamation-3440200.zip
+        URL https://www.sqlite.org/2024/sqlite-amalgamation-3450000.zip
     )
 
     FetchContent_MakeAvailable(sqlite)
