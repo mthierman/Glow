8cdf0543d6dd06ebc831019b67b4168f2d0cb239
mthierman <mthierman@gmail.com>
2024-02-15 16:31:39 -0700

    Update CMake to bring in line with Airglow

--- cmake/Packages.cmake
+++ cmake/Packages.cmake
@@ -1,7 +1,3 @@
-set(FETCHCONTENT_SOURCE_DIR_CURL
-    "$ENV{LIBRARIES}/curl"
-)
-
 set(FETCHCONTENT_SOURCE_DIR_JSON
     "$ENV{LIBRARIES}/json"
 )
@@ -10,28 +6,20 @@ set(FETCHCONTENT_SOURCE_DIR_SQLITE
     "$ENV{LIBRARIES}/sqlite-amalgamation-3450100"
 )
 
-set(FETCHCONTENT_SOURCE_DIR_TOML
-    "$ENV{LIBRARIES}/tomlplusplus"
-)
-
-set(GLOW_RESTORE_NUGET
-    OFF
-)
-
-cmake_path(
-    SET
-    webview2_SOURCE_DIR
-    "$ENV{NUGET_PACKAGES}/microsoft.web.webview2/1.0.2277.86"
-)
-
 cmake_path(
     SET
     wil_SOURCE_DIR
-    "$ENV{NUGET_PACKAGES}/microsoft.windows.implementationlibrary/1.0.240122.1"
+    "$ENV{NUGET}/Microsoft.Windows.ImplementationLibrary.1.0.240122.1"
 )
 
 cmake_path(
     SET
     cppwinrt_SOURCE_DIR
-    "$ENV{NUGET_PACKAGES}/microsoft.windows.cppwinrt/2.0.240111.5"
+    "$ENV{NUGET}/Microsoft.Windows.CppWinRT.2.0.240111.5"
+)
+
+cmake_path(
+    SET
+    webview2_SOURCE_DIR
+    "$ENV{NUGET}/Microsoft.Web.WebView2.1.0.2277.86"
 )
