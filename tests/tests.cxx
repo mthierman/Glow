337ab618b34d6b581ac4f6d8d00b24eec09d3e71
mthierman <mthierman@gmail.com>
2024-04-29 20:24:00 -0600

    Add colors test

--- tests/tests.cxx
+++ tests/tests.cxx
@@ -9,4 +9,22 @@
 #include <glow/config.hxx>
 #include <glow/glow.hxx>
 
-auto main(int argc, char* argv[]) -> int { return 0; }
+#include <print>
+
+auto main(int argc, char* argv[]) -> int
+{
+    glow::Colors colors;
+
+    std::println("{}", colors.string.accent);
+    std::println("{}", colors.string.accentDark1);
+    std::println("{}", colors.string.accentDark2);
+    std::println("{}", colors.string.accentDark3);
+    std::println("{}", colors.string.accentLight1);
+    std::println("{}", colors.string.accentLight2);
+    std::println("{}", colors.string.accentLight3);
+
+    std::println("{}", colors.string.foreground);
+    std::println("{}", colors.string.background);
+
+    return 0;
+}
