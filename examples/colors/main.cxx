2023f534fa695972d55f7998f33a245c4bdd3089
mthierman <mthierman@gmail.com>
2024-07-11 13:27:19 -0600

    Add color module

---
+++ examples/colors/main.cxx
@@ -0,0 +1,4 @@
+#include <stdlib.h>
+#include <print>
+
+auto main() -> int { return EXIT_SUCCESS; }
---
+++ tools/templates/template.cxx
@@ -0,0 +1,9 @@
+// clang-format off
+// Glow - https://github.com/mthierman/Glow
+// SPDX-FileCopyrightText: © 2024 Mike Thierman <mthierman@gmail.com>
+// SPDX-License-Identifier: MIT
+// clang-format on
+
+// #include <glow/.hxx>
+
+namespace glow::temp { };
