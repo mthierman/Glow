643a9ad2f2ae12bb0d14b0acd94afe2b313aef11
mthierman <mthierman@gmail.com>
2024-07-11 14:03:14 -0600

    Color example

--- examples/colors/main.cxx
+++ examples/colors/main.cxx
@@ -2,5 +2,12 @@
 #include <stdlib.h>
 
 #include <glow/color.hxx>
+#include <glow/system.hxx>
 
-auto main() -> int { return EXIT_SUCCESS; }
+auto main() -> int {
+    auto uiSettings { glow::system::get_ui_settings() };
+    auto color { glow::color::get_color(uiSettings, winrt::UIColorType::Accent) };
+    glow::system::dbg("{}", color);
+
+    return EXIT_SUCCESS;
+}
