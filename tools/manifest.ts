f3615eaeee8c3a1fcc66eaaa6220f84d372a9fa1
mthierman <mthierman@gmail.com>
2024-07-11 21:38:08 -0600

    Switch to async/await

--- tools/manifest.ts
+++ tools/manifest.ts
@@ -1,32 +1,23 @@
 import { execSync } from "node:child_process";
-import { rm } from "node:fs";
-import { mkdir, writeFile } from "node:fs/promises";
+import { mkdir, rm, writeFile } from "node:fs/promises";
 import { resolve } from "node:path";
 import manifest from "../package.json" with { type: "json" };
 
+const makeManifest = () => {
+    return {
+        name: `${manifest.fullname}`,
+        description: `${manifest.description}`,
+        version: `${manifest.version}`,
+        date: `${new Date().toLocaleDateString("en-GB", { day: "2-digit", month: "2-digit", year: "2-digit" })} ${new Date().toLocaleTimeString("en-GB", { hour12: false })}`,
+        hash: execSync("git rev-parse HEAD").toString().trim(),
+        symbol: "🌟",
+        github: "https://github.com/mthierman/Glow",
+        download: `https://github.com/mthierman/Glow/releases/download/v${manifest.version}/Glow.zip`,
+    };
+};
+
 const outdir = resolve(import.meta.dirname, "..", "build");
 
-mkdir(outdir, { recursive: true })
-    .then(async () => {
-        await writeFile(
-            resolve(outdir, `manifest.json`),
-            JSON.stringify(
-                {
-                    name: `${manifest.fullname}`,
-                    description: `${manifest.description}`,
-                    version: `${manifest.version}`,
-                    date: `${new Date().toLocaleDateString("en-GB", { day: "2-digit", month: "2-digit", year: "2-digit" })} ${new Date().toLocaleTimeString("en-GB", { hour12: false })}`,
-                    hash: execSync("git rev-parse HEAD").toString().trim(),
-                    symbol: "🌟",
-                    github: "https://github.com/mthierman/Glow",
-                    download: `https://github.com/mthierman/Glow/releases/download/v${manifest.version}/Glow.zip`,
-                },
-                null,
-                4,
-            ),
-        );
-    })
-    .catch(() => process.exit(1))
-    .finally(() => {
-        rm(resolve(import.meta.dirname, "manifest.js"), () => {});
-    });
+await mkdir(outdir, { recursive: true });
+await writeFile(resolve(outdir, `manifest.json`), JSON.stringify(makeManifest(), null, 4));
+await rm(resolve(resolve(import.meta.dirname), `manifest.js`));
