e0962c0b5a787ef2758854927104774954fce8aa
mthierman <mthierman@gmail.com>
2024-07-22 14:32:40 -0600

    Add New-Manifest cmd

--- tools/PowerShell/PowerShell.psm1
+++ tools/PowerShell/PowerShell.psm1
@@ -1,3 +1,20 @@
+function New-Manifest
+{
+    if (!(Test-Path "build")) { New-Item -ItemType Directory "build" }
+    $manifest = Get-Content "package.json" | ConvertFrom-Json
+
+([PSCustomObject]@{
+        name        = $manifest.name
+        description = $manifest.description
+        version     = $manifest.version
+        date        = Get-Date -Format "yyyy'/'MM'/'dd HH:mm:ss"
+        hash        = $(git rev-parse HEAD)
+        symbol      = "🌟"
+        github      = "https://github.com/mthierman/Glow"
+        download    = "https://github.com/mthierman/Glow/releases/download/v$($manifest.version)/Glow.zip"
+    }) | ConvertTo-Json | Out-File -FilePath "build/manifest.json"
+}
+
 function Invoke-DevShell
 {
     [CmdletBinding()]
