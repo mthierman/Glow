b0ab92671a6da9b53ef9e04b56e4cd5d4aa39097
mthierman <mthierman@gmail.com>
2024-07-30 10:43:31 -0600

    Cleanup PowerShell scripts

--- tools/scripts/compress.ps1
+++ tools/scripts/compress.ps1
@@ -1,8 +1,2 @@
-function Compress-Glow
-{
-    [CmdletBinding()]
-    param ()
-
-    if (Test-Path "build/Glow.zip") { Remove-Item "build/Glow.zip" -Force }
-    7z a build/Glow.zip * -xr'!build' -xr'!node_modules' -xr'!.git' -xr'!.vscode' -xr'!CMakeUserPresets.json'
-}
+if (Test-Path "build/Glow.zip") { Remove-Item "build/Glow.zip" -Force }
+7z a build/Glow.zip * -xr'!build' -xr'!node_modules' -xr'!.git' -xr'!.vscode' -xr'!CMakeUserPresets.json'
--- tools/scripts/publish.ps1
+++ tools/scripts/publish.ps1
@@ -1,15 +1,11 @@
-function Publish-Glow
-{
-    [CmdletBinding()]
-    param (
-        [ValidateNotNullOrEmpty()]
-        [string]$Branch = 'main'
-    )
+param (
+    [ValidateNotNullOrEmpty()]
+    [string]$Branch = 'main'
+)
 
-    $version = "v$((Get-Content "build/manifest.json" | ConvertFrom-Json).version)"
-    $notes = Get-Item "build/notes/release_notes"
-    $archive = Get-Item "build/Glow.zip"
+$version = "v$((Get-Content "build/manifest.json" | ConvertFrom-Json).version)"
+$notes = Get-Item "build/notes/release_notes"
+$archive = Get-Item "build/Glow.zip"
 
-    gh release delete $version -y
-    gh release create $version $archive --notes-file $notes -t $version
-}
+gh release delete $version -y
+gh release create $version $archive --notes-file $notes -t $version
