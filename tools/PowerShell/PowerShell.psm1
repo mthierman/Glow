68adc3df34549f3a14d11d246b613ec25a3eaf74
mthierman <mthierman@gmail.com>
2024-07-30 10:41:51 -0600

    Cleanup PowerShell and Workflows

--- tools/PowerShell/PowerShell.psm1
+++ tools/PowerShell/PowerShell.psm1
@@ -1,57 +1,3 @@
-function Invoke-DevShell
-{
-    [CmdletBinding()]
-    param (
-        [ValidateSet('x86', 'amd64', 'arm', 'arm64')]
-        [string]$Arch = 'amd64',
-        [ValidateSet('x86', 'amd64')]
-        [string]$HostArch = 'amd64'
-    )
-
-    $vswhere = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
-    $vspath = & $vswhere -products * -latest -property installationPath
-    & "$vspath\Common7\Tools\Launch-VsDevShell.ps1" -Arch $Arch -HostArch $HostArch -SkipAutomaticLocation
-}
-
-function Invoke-CMake
-{
-    [CmdletBinding()]
-    param (
-        [ValidateSet('Development', 'CI')]
-        [string]$ConfigurePreset = ($env:CI) ? 'CI' : 'Development',
-        [ValidateSet('Debug', 'Release', 'CI')]
-        [string]$BuildPreset = ($env:CI) ? 'CI' : 'Debug'
-    )
-
-    cmake --preset $ConfigurePreset
-    cmake --build --preset $BuildPreset
-}
-
-function Invoke-CTest
-{
-    [CmdletBinding()]
-    param (
-        [ValidateNotNullOrEmpty()]
-        [string]$TestPreset = ($env:CI) ? 'CI' : 'Debug',
-        [switch]$JSON,
-        [switch]$XML
-    )
-
-    $arguments = @("--preset", $TestPreset)
-
-    if ($JSON)
-    {
-        $arguments += "--show-only=json-v1"
-    }
-
-    if ($XML)
-    {
-        $arguments += "--output-junit", "tests/log.xml"
-    }
-
-    &ctest @arguments
-}
-
 function New-GlowManifest
 {
     if (!(Test-Path "build")) { New-Item -ItemType Directory "build" }
@@ -86,23 +32,10 @@ function Publish-Glow
         [string]$Branch = 'main'
     )
 
-    # $hash = Get-Content "build/notes/short_hash"
-    # $version = Get-Content "build/notes/version"
-    # $notes = Get-Item "build/notes/release_notes"
-
     $version = "v$((Get-Content "build/manifest.json" | ConvertFrom-Json).version)"
     $notes = Get-Item "build/notes/release_notes"
     $archive = Get-Item "build/Glow.zip"
 
     gh release delete $version -y
     gh release create $version $archive --notes-file $notes -t $version
-}
-
-function Test-GlowCI
-{
-    pnpm manifest
-    Invoke-DevShell
-    Invoke-CMake
-    Compress-Glow
-    Invoke-CTest
 }
