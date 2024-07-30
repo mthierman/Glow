788dba605c069a5098b321992dfba441affcf0e0
mthierman <mthierman@gmail.com>
2024-07-30 10:30:25 -0600

    Add deps module

---
+++ cmake/deps.cmake
@@ -0,0 +1,50 @@
+include(FetchContent)
+
+FetchContent_Declare(
+    cmake-modules
+    GIT_REPOSITORY "https://github.com/mthierman/cmake-modules.git"
+    GIT_TAG main
+    GIT_SHALLOW ON
+    )
+
+FetchContent_MakeAvailable(cmake-modules)
+
+list(
+    APPEND
+    CMAKE_MODULE_PATH
+    "${cmake-modules_SOURCE_DIR}"
+    )
+
+include(cmake-modules)
+
+fetch_common()
+
+fetch_wil(
+    VERSION
+    1.0.240122.1
+    )
+
+fetch_cppwinrt(
+    VERSION
+    2.0.240405.15
+    )
+
+fetch_webview2(
+    VERSION
+    1.0.2592.51
+    )
+
+fetch_ada(
+    VERSION
+    2.8.0
+    )
+
+fetch_json(
+    VERSION
+    3.11.3
+    )
+
+fetch_sqlite(
+    VERSION
+    3460000
+    )
