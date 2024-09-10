b01dd924f0c1e53c52837a3b53c60acc68400301
mthierman <mthierman@gmail.com>
2024-09-10 00:07:46 -0600

    Cleanup console example

--- examples/console/main.cxx
+++ examples/console/main.cxx
@@ -1,14 +1,3 @@
 #include <stdlib.h>
 
-#include <filesystem>
-#include <glow/filesystem.hxx>
-#include <glow/log.hxx>
-#include <glow/std.hxx>
-
-auto main() -> int {
-    // std::filesystem::path path { L"" };
-    auto path { glow::filesystem::known_folder() };
-    glow::log::log(std::format("{}", path));
-
-    return EXIT_SUCCESS;
-}
+auto main() -> int { return EXIT_SUCCESS; }
