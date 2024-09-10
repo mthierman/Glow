aa16a5529a171faa3cd9b471a5f22a95e5f44f54
mthierman <mthierman@gmail.com>
2024-09-10 17:42:25 -0600

    Cleanup main

--- examples/console/main.cxx
+++ examples/console/main.cxx
@@ -6,26 +6,6 @@
 
 auto main() -> int {
     //
-    auto ui { glow::system::get_ui_settings() };
-    auto color { glow::color::get_color(ui) };
-
-    // auto s { glow::color::to_string(color) };
-    // auto w { glow::color::to_wstring(color) };
-
-    // std::println("{}", s);
-    // std::println(L"{}", w);
-
-    auto s { std::format("{}", color) };
-    auto w { std::format(L"{}", color) };
-
-    // std::println("{}, {}, {}, {}", color.R, color.G, color.B, color.A);
-    // std::println("{}", glow::color::to_hex(color));
-
-    // std::println("{}", glow::color::to_string(color));
-    // std::println(L"{}", glow::color::to_wstring(color));
-
-    // std::println("{}", color);
-    // std::println(L"{}", color);
 
     return EXIT_SUCCESS;
 }
