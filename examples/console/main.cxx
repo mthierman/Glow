5dae984ee71dbf7921446e8a005288cabaa16b4a
mthierman <mthierman@gmail.com>
2024-09-09 23:58:24 -0600

    Cleanup console example

--- examples/console/main.cxx
+++ examples/console/main.cxx
@@ -1,17 +1,3 @@
-#include <print>
 #include <stdlib.h>
 
-#include <glow/filesystem.hxx>
-#include <glow/log.hxx>
-
-auto main() -> int {
-    auto knownFolder { glow::filesystem::known_folder(FOLDERID_Documents, { "test", "test" }) };
-
-    // glow::log::log("{}", glow::text::utf16_to_utf8(glow::filesystem::temp_folder().wstring()));
-    // std::vformat(L"", knownFolder.wstring());
-    // glow::log::println("{}", knownFolder);
-
-    glow::log::log(glow::filesystem::to_string(knownFolder));
-
-    return EXIT_SUCCESS;
-}
+auto main() -> int { return EXIT_SUCCESS; }
