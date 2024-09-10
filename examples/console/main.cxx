947b2b5aa0ba52c562de48d7b42d875e3167a561
mthierman <mthierman@gmail.com>
2024-09-10 17:15:16 -0600

    Cleanup text

--- examples/console/main.cxx
+++ examples/console/main.cxx
@@ -1,16 +1,7 @@
 #include <stdlib.h>
 
-#include <print>
-#include <string>
-
-#include <glow/filesystem.hxx>
-#include <glow/text.hxx>
-#include <glow/std.hxx>
-
 auto main() -> int {
-    auto knownFolder { glow::filesystem::known_folder() };
-
-    std::println("{}", knownFolder);
+    //
 
     return EXIT_SUCCESS;
 }
--- src/include/glow/text.hxx
+++ src/include/glow/text.hxx
@@ -13,7 +13,6 @@ auto to_string(std::wstring_view input) -> std::string;
 auto to_wstring(std::string_view input) -> std::wstring;
 auto to_string(std::u8string_view input) -> std::string;
 auto to_wstring(std::u8string_view input) -> std::wstring;
-
 auto to_u8string(std::string_view input) -> std::u8string;
 auto to_u8string(std::wstring_view input) -> std::u8string;
 }; // namespace glow::text
