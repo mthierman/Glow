059aa20c47e6c7315ba92a72fa5ae17fd57bd196
mthierman <mthierman@gmail.com>
2024-09-12 14:04:42 -0600

    Cleanup window example

--- examples/window/main.cxx
+++ examples/window/main.cxx
@@ -1,56 +1,9 @@
 #include <glow/glow.hxx>
 
-enum struct glow::message::Code : ::UINT { HELLO, GOODBYE };
-namespace wm = glow::message::wm;
-
-struct App final : glow::app::App {
-    App() {
-        messages.on(WM_NOTIFY, [](wm::NOTIFY msg) {
-            switch (msg.notification().code) {
-                case glow::message::Code::HELLO: {
-                    glow::log::log("HELLO: {}", msg.notification().message);
-                } break;
-                case glow::message::Code::GOODBYE: {
-                    glow::log::log("GOODBYE: {}", msg.notification().message);
-                } break;
-            }
-
-            return 0;
-        });
-
-        create();
-    }
-};
-
 struct Window final : glow::window::Window {
     Window() {
         messages.on(WM_DESTROY, [](wm::DESTROY msg) {
             msg.quit();
-
-            return 0;
-        });
-
-        messages.on(WM_KEYDOWN, [this](wm::KEYDOWN msg) {
-            switch (msg.key()) {
-                case VK_F11: {
-                    if (!states.fullscreen) {
-                        states.fullscreen = enable_fullscreen();
-                    } else {
-                        states.fullscreen = disable_fullscreen();
-                    }
-                } break;
-
-                case VK_F4: {
-                    if (states.centered) {
-                        set_position(positions.restoreCentered);
-                        states.centered = false;
-                    } else {
-                        positions.restoreCentered = positions.window;
-                        center();
-                        states.centered = true;
-                    }
-                } break;
-            }
 
             return 0;
         });
@@ -61,22 +14,7 @@ struct Window final : glow::window::Window {
 };
 
 auto main() -> int {
-    App app;
-
     Window window;
-
-    window.notify_app(glow::message::Code::HELLO, "TEST1");
-    window.notify_app(glow::message::Code::GOODBYE, "TEST2");
-
-    window.set_background(glow::window::Background::Custom);
-    window.set_background_color(glow::color::system(winrt::UIColorType::Accent));
-    window.set_border_color(glow::color::system(winrt::UIColorType::Accent));
-    window.set_caption_color(glow::color::system(winrt::UIColorType::Accent));
-    window.set_text_color(glow::color::system(winrt::UIColorType::Foreground));
-
-    window.enable_dark_mode();
-
-    window.set_backdrop(DWM_SYSTEMBACKDROP_TYPE::DWMSBT_MAINWINDOW);
 
     return glow::app::run();
 }
