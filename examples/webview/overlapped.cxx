d240c4d485cea36a5cc50f0e53f29688f848263d
mthierman <mthierman@gmail.com>
2024-09-13 16:45:47 -0600

    Testing 2 webview event handlers

--- examples/webview/overlapped.cxx
+++ examples/webview/overlapped.cxx
@@ -16,16 +16,38 @@ auto main() -> int {
     webView.create([]() {
         webView.navigate("https://localhost:5173/");
 
-        ::EventRegistrationToken token;
-        webView.core->add_NavigationCompleted(
-            wil::MakeAgileCallback<ICoreWebView2NavigationCompletedEventHandler>(
+        // ::EventRegistrationToken token;
+        // webView.core->add_NavigationCompleted(
+        //     wil::MakeAgileCallback<ICoreWebView2NavigationCompletedEventHandler>(
+        //         [](ICoreWebView2* sender,
+        //            ICoreWebView2NavigationCompletedEventArgs* args) -> ::HRESULT {
+        //     webView.activate();
+
+        //     return S_OK;
+        // }).Get(),
+        //     &token);
+
+        ::EventRegistrationToken token1;
+        webView.core->add_DOMContentLoaded(
+            wil::MakeAgileCallback<ICoreWebView2DOMContentLoadedEventHandler>(
                 [](ICoreWebView2* sender,
-                   ICoreWebView2NavigationCompletedEventArgs* args) -> ::HRESULT {
+                   ICoreWebView2DOMContentLoadedEventArgs* args) -> ::HRESULT {
             webView.activate();
 
             return S_OK;
         }).Get(),
-            &token);
+            &token1);
+
+        ::EventRegistrationToken token2;
+        webView.core->add_DOMContentLoaded(
+            wil::MakeAgileCallback<ICoreWebView2DOMContentLoadedEventHandler>(
+                [](ICoreWebView2* sender,
+                   ICoreWebView2DOMContentLoadedEventArgs* args) -> ::HRESULT {
+            glow::log::log("TEST");
+
+            return S_OK;
+        }).Get(),
+            &token2);
     });
 
     return glow::app::run();
