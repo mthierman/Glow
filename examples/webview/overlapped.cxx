d05a306df96037da85a45f8efb63cf8ea0761277
mthierman <mthierman@gmail.com>
2024-09-14 02:20:41 -0600

    Cleanup example

--- examples/webview/overlapped.cxx
+++ examples/webview/overlapped.cxx
@@ -14,20 +14,24 @@ auto main() -> int {
     webView.set_background_color({ 227, 178, 60 });
     webView.set_background_style(glow::window::BackgroundStyle::Custom);
 
-    // webView.core->add_DOMContentLoaded(
-    //     webView
-    //         .handler<ICoreWebView2DOMContentLoadedEventHandler>(
-    //             [](ICoreWebView2* /* sender */,
-    //                ICoreWebView2DOMContentLoadedEventArgs* /* args */) -> ::HRESULT {
-    //     webView.activate();
-    //     webView.core->remove_DOMContentLoaded(webView.tokens("DOMContentLoaded"));
-
-    //     return S_OK;
-    // }).Get(),
-    //     &webView.tokens("DOMContentLoaded"));
-
     // webView.create();
-    webView.create([]() { webView.navigate("https://localhost:5173/"); }, true);
+
+    webView.create([]() {
+        webView.core->add_DOMContentLoaded(
+            webView
+                .handler<ICoreWebView2DOMContentLoadedEventHandler>(
+                    [](ICoreWebView2* /* sender */,
+                       ICoreWebView2DOMContentLoadedEventArgs* /* args */) -> ::HRESULT {
+            glow::log::log("DOMContentLoaded");
+            webView.core->remove_DOMContentLoaded(webView.tokens("DOMContentLoaded"));
+
+            return S_OK;
+        }).Get(),
+            &webView.tokens("DOMContentLoaded"));
+
+        webView.navigate("https://localhost:5173/");
+        // webView.navigate("https://google.com/");
+    }, true);
 
     return glow::app::run();
 }
