99ac19a3b060bce7ba3b8be3d8adbd097894a41a
mthierman <mthierman@gmail.com>
2024-09-13 20:48:49 -0600

    Add Color struct

--- src/include/glow/color.hxx
+++ src/include/glow/color.hxx
@@ -21,6 +21,13 @@ using namespace winrt::Windows::UI::ViewManagement;
 }; // namespace winrt
 
 namespace glow::color {
+struct Color {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+    uint8_t a;
+};
+
 auto create_brush(uint8_t r, uint8_t g, uint8_t b) -> ::HBRUSH;
 auto create_brush(const winrt::Color& color) -> ::HBRUSH;
 
