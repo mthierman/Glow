04fc983fc0dd0b36c59bf3a242c2020355c700c6
mthierman <mthierman@gmail.com>
2024-09-14 15:23:27 -0600

    Add background handling to overlapped example

--- examples/window/overlapped.cxx
+++ examples/window/overlapped.cxx
@@ -10,8 +10,15 @@ auto main() -> int {
     });
 
     window.create();
-    window.background_style(glow::window::BackgroundStyle::Custom);
-    window.background_custom(glow::color::Color(255, 0, 0));
+
+    // window.background_style(glow::window::BackgroundStyle::Transparent);
+
+    // window.background_style(glow::window::BackgroundStyle::System);
+    // window.background_dark(glow::color::Color(50, 50, 50));
+    // window.background_light(glow::color::Color(200, 200, 200));
+
+    // window.background_style(glow::window::BackgroundStyle::Custom);
+    // window.background_custom(glow::color::Color(255, 0, 0));
 
     return glow::app::run();
 }
