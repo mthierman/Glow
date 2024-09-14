c8f6567ffd42b86a2e0e4e5eda517bad3e4ef25f
mthierman <mthierman@gmail.com>
2024-09-13 23:40:41 -0600

    Cleanup console

--- examples/console/main.cxx
+++ examples/console/main.cxx
@@ -1,24 +1,3 @@
-#ifdef WIN32_LEAN_AND_MEAN
-#undef WIN32_LEAN_AND_MEAN
-#include <Windows.h>
-#include <gdiplus.h>
-#include <gdipluscolor.h>
-#include <gdipluspixelformats.h>
-#endif
-
 #include <stdlib.h>
 
-#include <print>
-
-#include <glow/glow.hxx>
-
-auto main() -> int {
-    glow::system::GdiPlus gdiPlus;
-
-    auto color { Gdiplus::Color() };
-    color.SetFromCOLORREF(RGB(255, 0, 0));
-
-    std::println("{}", color.GetR());
-
-    return EXIT_SUCCESS;
-}
+auto main() -> int { return EXIT_SUCCESS; }
