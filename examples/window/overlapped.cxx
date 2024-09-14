b75bddfdf196b40aadecd7c55e1af8af5a21355f
mthierman <mthierman@gmail.com>
2024-09-14 15:44:07 -0600

    Checkpoint

--- examples/window/overlapped.cxx
+++ examples/window/overlapped.cxx
@@ -11,14 +11,16 @@ auto main() -> int {
 
     window.create();
 
-    // window.background_style(glow::window::BackgroundStyle::Transparent);
+    // window.set_title("TEST");
 
-    // window.background_style(glow::window::BackgroundStyle::System);
+    // window.background_style(glow::window::Background::Style::Transparent);
+
+    // window.background_style(glow::window::Background::Style::System);
     // window.background_dark(glow::color::Color(50, 50, 50));
     // window.background_light(glow::color::Color(200, 200, 200));
 
-    window.background_style(glow::window::Background::Style::Custom);
-    window.background_custom(glow::color::Color(255, 0, 0));
+    // window.background_style(glow::window::Background::Style::Custom);
+    // window.background_custom(glow::color::Color(255, 0, 0));
 
     return glow::app::run();
 }
