099e18d9b57283fa7918ac482cda4b262b44fcd5
mthierman <mthierman@gmail.com>
2024-09-13 22:07:22 -0600

    Gdi

--- examples/console/main.cxx
+++ examples/console/main.cxx
@@ -1,9 +1,24 @@
+#ifdef WIN32_LEAN_AND_MEAN
+#undef WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <gdiplus.h>
+#include <gdipluscolor.h>
+#include <gdipluspixelformats.h>
+#endif
+
 #include <stdlib.h>
+
+#include <print>
 
 #include <glow/glow.hxx>
 
 auto main() -> int {
-    //
+    glow::system::GdiPlus gdiPlus;
+
+    auto color { Gdiplus::Color() };
+    color.SetFromCOLORREF(RGB(255, 0, 0));
+
+    std::println("{}", color.GetR());
 
     return EXIT_SUCCESS;
 }
--- src/include/glow/color.hxx
+++ src/include/glow/color.hxx
@@ -8,8 +8,6 @@
 
 #include <Windows.h>
 
-#include <gdipluscolor.h>
-
 #include <format>
 #include <string>
 
