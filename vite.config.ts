24513f72e02a72d2b9f9f3346f98edc18c13ff8f
mthierman <mthierman@gmail.com>
2024-09-14 19:58:15 -0600

    Fix vite outdir

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,12 @@ import { CommonServerOptions, UserConfig, defineConfig } from "vite";
 
 const userConfig: UserConfig = {
     root: "./examples/webview/vite",
-};
-
-const commonServerOptions: CommonServerOptions = {
-    https: {
-        pfx: resolve("../.cert/localhost.pfx"),
-        passphrase: "localhost",
+    build: {
+        outDir: resolve(import.meta.dirname, "dist"),
     },
 };
+
+const commonServerOptions: CommonServerOptions = {};
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
