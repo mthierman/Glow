5ae4f0e0f10f14080c84b8108222a3223ee2d968
mthierman <mthierman@gmail.com>
2024-09-16 16:24:04 -0600

    Fix example

--- examples/console/config.cxx
+++ examples/console/config.cxx
@@ -14,23 +14,18 @@ auto main(/* int argc, char* argv[] */) -> int {
 
     auto config { glow::config::Config(filePath) };
 
-    config.set<std::u8string>(u8"string", u8"wrapper");
+    config.set<std::u8string>(u8"u8string", u8"wrapper");
     config.set<bool>(u8"bool", true);
     config.set<double>(u8"number", 24);
 
-    // auto value { config.get<std::u8string>(u8"string") };
-    // auto value { config.get<std::u8string>(u8"string") };
-    // auto value { config.get<std::u8string>(u8"string") };
-    // glow::log::log("{}", glow::text::to_string(value));
-
     glow::log::log("{}, {}, {}",
-                   config.get<std::u8string>(u8"string"),
+                   config.get<std::u8string>(u8"u8string"),
                    config.get<bool>(u8"bool"),
                    config.get<double>(u8"number"));
 
     config.save();
 
-    // config.load();
+    config.load();
 
     return EXIT_SUCCESS;
 }
