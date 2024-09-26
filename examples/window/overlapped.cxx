7be93fa9ce59639c03c65b0069c58dab843dd3a1
mthierman <mthierman@gmail.com>
2024-09-26 10:51:23 -0600

    Cleanup overlapped example

--- examples/window/overlapped.cxx
+++ examples/window/overlapped.cxx
@@ -3,22 +3,11 @@
 glow::window::Overlapped window;
 
 auto main() -> int {
-    window.messages.on(WM_CREATE, [](glow::message::wm::CREATE /* msg */) {
-        glow::log::err("DESTROY");
+    window.messages.on(WM_DESTROY, [](glow::message::wm::DESTROY msg) {
+        msg.quit();
 
         return 0;
     });
-
-    window.messages.on(WM_DESTROY, [](glow::message::wm::DESTROY /* msg */) {
-        // msg.quit();
-        ::PostQuitMessage(0);
-        glow::log::err("DESTROY");
-
-        return 0;
-    });
-
-    window.min_size(200, 200);
-    // window.max_size(400, 400);
 
     window.create();
 
