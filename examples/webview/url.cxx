78169b0e32365d2d9d33a4bc0dc8e6cde3d5e19b
mthierman <mthierman@gmail.com>
2024-09-26 13:06:37 -0600

    Add URL example

---
+++ examples/webview/url.cxx
@@ -0,0 +1,35 @@
+#include <glow/glow.hxx>
+
+struct WebView final : glow::window::WebView {
+    WebView() {
+        messages.on(WM_DESTROY, [](glow::message::wm::DESTROY msg) {
+            msg.quit();
+
+            return 0;
+        });
+
+        create([this]() {
+            if (core) {
+                core->add_SourceChanged(
+                    event.make<ICoreWebView2SourceChangedEventHandler>(
+                        [this](ICoreWebView2* sender,
+                               ICoreWebView2SourceChangedEventArgs* args [[maybe_unused]]) {
+                    wil::unique_cotaskmem_string uri;
+                    sender->get_Source(&uri);
+                    set_title(uri.get());
+
+                    return S_OK;
+                }),
+                    event.token("SourceChanged"));
+            }
+
+            navigate("http://localhost:5173/");
+        });
+    }
+};
+
+auto main() -> int {
+    WebView webView;
+
+    return glow::message::run_loop();
+}
