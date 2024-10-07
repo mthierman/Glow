ab5b2ae9a336134f8189bb653d32cd5056a45200
mthierman <mthierman@gmail.com>
2024-10-07 16:30:36 -0600

    Fix wmain

--- examples/programs/main.cxx
+++ examples/programs/main.cxx
@@ -8,14 +8,8 @@
 
 #include <glow/glow.hxx>
 
-// auto main(/* int argc, char* argv[] */) -> int {
-//     std::println("Console example:");
-
-//     return EXIT_SUCCESS;
-// }
-
 auto wmain([[maybe_unused]] int argc, [[maybe_unused]] wchar_t* argv[]) -> int {
-    //
+    std::println("Console example:");
 
     return EXIT_SUCCESS;
 }
