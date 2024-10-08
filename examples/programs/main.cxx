e4f907e3ff48e893c4ae27ab677516406d771ffc
mthierman <mthierman@gmail.com>
2024-10-07 21:05:57 -0600

    Clear program example

--- examples/programs/main.cxx
+++ examples/programs/main.cxx
@@ -2,24 +2,12 @@
 
 #include <stdlib.h>
 
-#include <format>
-#include <iostream>
-#include <print>
-
 #include <glow/glow.hxx>
 
+#include <print>
+
 auto wmain([[maybe_unused]] int argc, [[maybe_unused]] wchar_t* argv[]) -> int {
-    std::println("Console example:");
-
-    auto errorCode { S_OK };
-    glow::log::log(errorCode);
-
-    auto lambdaFunc = []() {
-        auto errorCode { S_OK };
-        glow::log::log(errorCode);
-    };
-
-    lambdaFunc();
+    //
 
     return EXIT_SUCCESS;
 }
