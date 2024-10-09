aa4d4e7b2f141eaa2e804ef246d0b58d9bd8f069
mthierman <mthierman@gmail.com>
2024-10-09 01:12:26 -0600

    Cleanup program

--- examples/programs/main.cxx
+++ examples/programs/main.cxx
@@ -8,14 +8,5 @@
 #include <glow/glow.hxx>
 
 auto wmain([[maybe_unused]] int argc, [[maybe_unused]] wchar_t* argv[]) -> int {
-    std::optional<int> opt;
-
-    opt = 32;
-
-    if (auto test { opt })
-        std::println("{}", *opt);
-    else
-        std::println("FAIL!");
-
     return EXIT_SUCCESS;
 }
