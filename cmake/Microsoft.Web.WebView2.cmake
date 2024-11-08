857e833ca8897bb82d6ccd5556299a2246915e91
mthierman <mthierman@gmail.com>
2024-11-08 12:39:55 -0700

    Update webview2 version

--- cmake/Microsoft.Web.WebView2.cmake
+++ cmake/Microsoft.Web.WebView2.cmake
@@ -1,6 +1,6 @@
 FetchContent_Declare(
     Microsoft.Web.WebView2
-    URL "https://www.nuget.org/api/v2/package/Microsoft.Web.WebView2/1.0.2792.45"
+    URL "https://www.nuget.org/api/v2/package/Microsoft.Web.WebView2/1.0.2849.39"
     DOWNLOAD_NO_PROGRESS TRUE
     SOURCE_SUBDIR
     "NULL"
